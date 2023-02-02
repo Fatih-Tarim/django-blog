953a52f6f190a76c051616451e9bd6e65abb49af
Fatih-Tarim <fatihtrm99@gmail.com>
2023-02-02 15:38:46 +0300

    Yazı detay sayfası | Yorumlar

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path
-from blogapp.views import iletisim, anasayfa, kategori, yazilarim
+from blogapp.views import iletisim, anasayfa, kategori, yazilarim, detay
 
 urlpatterns = [
     path('', anasayfa, name='anasayfa'),
     path('iletisim', iletisim, name='iletisim'),
     path('kategori/<slug:kategoriSlug>', kategori, name='kategori'),
-    path('yazilarim', yazilarim, name='yazilarim')
+    path('yazilarim', yazilarim, name='yazilarim'),
+    path('detay/<slug:slug>', detay, name='detay')
 
 ]
