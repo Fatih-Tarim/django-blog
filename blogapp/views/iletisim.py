a96d989bf7a61b6b482a0a3569ca6dfb957e776e
Fatih-Tarim <fatihtrm99@gmail.com>
2023-02-03 00:16:19 +0300

    Form begin

--- blogapp/views/iletisim.py
+++ blogapp/views/iletisim.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from blogapp.forms import IletisimForm
+from blogapp.models import IletisimModel
 
 def iletisim(request):
+    form = IletisimForm()
+    if request.method == 'POST':
+        form = IletisimForm(request.POST)
+        if form.is_valid():
+            iletisim = IletisimModel()
+            iletisim.email = form.cleaned_data['email']
+            iletisim.isim_soyisim = form.cleaned_data['isim_soyisim']
+            iletisim.mesaj = form.cleaned_data['mesaj']
+            iletisim.save()
+            return redirect('anasayfa')
+
     context = {
-        'key' : 'başlık içerik'
+        'form' : form
     }
     return render(request, 'pages/iletisim.html', context=context)
 
