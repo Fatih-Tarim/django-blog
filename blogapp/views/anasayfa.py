17414d434f140cdf5327bfb7bbdd97cfb1eb22ec
Fatih-Tarim <fatihtrm99@gmail.com>
2023-02-02 14:48:46 +0300

    Search

--- blogapp/views/anasayfa.py
+++ blogapp/views/anasayfa.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from blogapp.models import YazilarModel
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 def anasayfa(request):
+    sorgu = request.GET.get('sorgu')
     yazilar = YazilarModel.objects.order_by('-id')
+    if sorgu:
+        yazilar = yazilar.filter(
+            Q(baslik__icontains=sorgu) | Q(icerik__icontains=sorgu)
+        ).distinct()
     sayfa = request.GET.get('sayfa')
     paginator = Paginator(yazilar,2)
     return render(request, 'pages/anasayfa.html', context={
