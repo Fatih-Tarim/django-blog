cea4f87ea7ee2d2f9d91067911632378669567d6
Fatih-Tarim <fatihtrm99@gmail.com>
2023-02-03 14:51:00 +0300

    Model form

--- blogapp/forms/iletisim.py
+++ blogapp/forms/iletisim.py
@@ -1,6 +1,15 @@
 from django import forms
+from blogapp.models import IletisimModel
 
-class IletisimForm(forms.Form):
-    email = forms.EmailField(required=True, max_length=100, label='E-Posta') 
-    isim_soyisim = forms.CharField(max_length=25, label='Ad Soyad')
-    mesaj = forms.CharField(widget=forms.Textarea(), label='Mesajınız')
+class IletisimForm(forms.ModelForm):
+    
+    class Meta:
+        model = IletisimModel
+        fields = ('isim_soyisim','email','mesaj')
+
+
+
+
+    # email = forms.EmailField(required=True, max_length=100, label='E-Posta') 
+    # isim_soyisim = forms.CharField(max_length=25, label='Ad Soyad')
+    # mesaj = forms.CharField(widget=forms.Textarea(), label='Mesajınız')
--- blogapp/views/iletisim.py
+++ blogapp/views/iletisim.py
@@ -4,15 +4,18 @@ from blogapp.forms import IletisimForm
 from blogapp.models import IletisimModel
 
 def iletisim(request):
-    form = IletisimForm()
+    form = IletisimForm(initial={
+        'email':'example@gmail.com'
+    })
     if request.method == 'POST':
         form = IletisimForm(request.POST)
         if form.is_valid():
-            iletisim = IletisimModel()
-            iletisim.email = form.cleaned_data['email']
-            iletisim.isim_soyisim = form.cleaned_data['isim_soyisim']
-            iletisim.mesaj = form.cleaned_data['mesaj']
-            iletisim.save()
+            # iletisim = IletisimModel()
+            # iletisim.email = form.cleaned_data['email']
+            # iletisim.isim_soyisim = form.cleaned_data['isim_soyisim']
+            # iletisim.mesaj = form.cleaned_data['mesaj']
+            # iletisim.save()
+            form.save()
             return redirect('anasayfa')
 
     context = {
