e113e8ea20460f4cbbb27b3aaa22404be18bb848
Fatih-Tarim <fatihtrm99@gmail.com>
2023-02-04 22:26:49 +0300

    Yorum sil

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from blogapp.views import iletisim, anasayfa, kategori, yazilarim, detay, yazi_ekle, yazi_guncelle, yazi_sil
+from blogapp.views import iletisim, anasayfa, kategori, yazilarim, detay, yazi_ekle, yazi_guncelle, yazi_sil, yorum_sil
 
 urlpatterns = [
     path('', anasayfa, name='anasayfa'),
@@ -10,5 +10,6 @@ urlpatterns = [
     path('detay/<slug:slug>', detay, name='detay'),
     path('yazi_ekle', yazi_ekle, name='yazi_ekle'),
     path('yazi_guncelle/<slug:slug>', yazi_guncelle, name='yazi_guncelle'),
-    path('yazi_sil/<slug:slug>', yazi_sil, name='yazi_sil')
+    path('yazi_sil/<slug:slug>', yazi_sil, name='yazi_sil'),
+    path('yorum_sil/<int:id>', yorum_sil, name='yorum_sil')
 ]
