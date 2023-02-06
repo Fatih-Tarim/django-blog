d470b04f3667b509b70821331b005e95f0d515f3
Fatih-Tarim <fatihtrm99@gmail.com>
2023-02-06 11:01:39 +0300

    Çıkış yap

--- config/urls.py
+++ config/urls.py
@@ -8,5 +8,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blogapp.urls'))
+    path('', include('blogapp.urls')),
+    path('account/', include('account.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
