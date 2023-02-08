0e3c8569ae86da8814a916ad241f380bc29d5b34
Fatih-Tarim <fatihtrm99@gmail.com>
2023-02-08 22:04:52 +0300

    Template & Redirect Views

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
 from blogapp.views import iletisim, anasayfa, kategori, yazilarim, detay, yazi_ekle, yazi_guncelle, yazi_sil, yorum_sil
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
     path('', anasayfa, name='anasayfa'),
@@ -11,5 +12,11 @@ urlpatterns = [
     path('yazi_ekle', yazi_ekle, name='yazi_ekle'),
     path('yazi_guncelle/<slug:slug>', yazi_guncelle, name='yazi_guncelle'),
     path('yazi_sil/<slug:slug>', yazi_sil, name='yazi_sil'),
-    path('yorum_sil/<int:id>', yorum_sil, name='yorum_sil')
+    path('yorum_sil/<int:id>', yorum_sil, name='yorum_sil'),
+    path('hakkimda', TemplateView.as_view(
+        template_name = 'pages/hakkimda.html'
+    ), name='hakkimda'),
+    path('yonlendir', RedirectView.as_view(
+        url = 'https://www.google.com'
+    ), name='yonlendir'),
 ]
