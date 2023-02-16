1a6764cc6374c17b267adde00d31737efc2602ee
Fatih-Tarim <fatihtrm99@gmail.com>
2023-02-16 16:18:24 +0300

    Delete view

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from blogapp.views import iletisim, anasayfa, KategoriListView, yazilarim, DetayView, yazi_ekle, yazi_guncelle, yazi_sil, yorum_sil
+from blogapp.views import iletisim, anasayfa, KategoriListView, yazilarim, DetayView, yazi_ekle, yazi_guncelle, YaziSilDeleteView, yorum_sil
 from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
@@ -11,7 +11,7 @@ urlpatterns = [
     path('detay/<slug:slug>', DetayView.as_view(), name='detay'),
     path('yazi_ekle', yazi_ekle, name='yazi_ekle'),
     path('yazi_guncelle/<slug:slug>', yazi_guncelle, name='yazi_guncelle'),
-    path('yazi_sil/<slug:slug>', yazi_sil, name='yazi_sil'),
+    path('yazi_sil/<slug:slug>', YaziSilDeleteView.as_view(), name='yazi_sil'),
     path('yorum_sil/<int:id>', yorum_sil, name='yorum_sil'),
     path('hakkimda', TemplateView.as_view(
         template_name = 'pages/hakkimda.html'
