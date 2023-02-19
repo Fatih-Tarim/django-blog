23e752c3643ab7743c3bee181f4c78c0459285f3
Fatih-Tarim <fatihtrm99@gmail.com>
2023-02-19 19:57:06 +0300

    Create View

--- blogapp/views/yazi_ekle.py
+++ blogapp/views/yazi_ekle.py
@@ -1,16 +1,38 @@
 from django.shortcuts import render, redirect
 from blogapp.forms import YaziEkleModelForm
 from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
+from blogapp.models import YazilarModel
+from django.urls import reverse
 
-@login_required(login_url='/')
-def yazi_ekle(request):
-    form = YaziEkleModelForm(request.POST or None, files=request.FILES or None)
-    if form.is_valid():
+
+class YaziEkleCreateView(CreateView):
+    template_name = 'pages/yazi_ekle.html'
+    model = YazilarModel
+    fields = ('baslik', 'icerik', 'resim', 'kategoriler')
+
+    def get_success_url(self):
+        return reverse('detay', kwargs={
+            'slug': self.object.slug
+        })
+
+    def form_valid(self, form):
         yazi = form.save(commit=False)
-        yazi.yazar = request.user
+        yazi.yazar = self.request.user
         yazi.save()
-        form.save_m2m() #Save many to many 
-        return redirect('detay', slug=yazi.slug)
-    return render(request, 'pages/yazi_ekle.html', context={
-        'form': form
-    })
+        form.save_m2m()
+        return super().form_valid(form)
+
+
+# @login_required(login_url='/')
+# def yazi_ekle(request):
+#     form = YaziEkleModelForm(request.POST or None, files=request.FILES or None)
+#     if form.is_valid():
+#         yazi = form.save(commit=False)
+#         yazi.yazar = request.user
+#         yazi.save()
+#         form.save_m2m() #Save many to many 
+#         return redirect('detay', slug=yazi.slug)
+#     return render(request, 'pages/yazi_ekle.html', context={
+#         'form': form
+#     })
