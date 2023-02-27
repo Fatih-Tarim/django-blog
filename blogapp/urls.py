48f77b2ae11e1895f61671f99343814c32748b33
Fatih-Tarim <fatihtrm99@gmail.com>
2023-02-27 10:41:15 +0300

    Email-Gönderildi

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -13,6 +13,9 @@ urlpatterns = [
     path('yazi_guncelle/<slug:slug>', YaziGuncelleUpdateView.as_view(), name='yazi_guncelle'),
     path('yazi_sil/<slug:slug>', YaziSilDeleteView.as_view(), name='yazi_sil'),
     path('yorum_sil/<int:id>', yorum_sil, name='yorum_sil'),
+    path("email-gonderildi/", TemplateView.as_view(
+        template_name = 'pages/email-gonderildi.html'
+    ), name="email-gonderildi"),
     path('hakkimda', TemplateView.as_view(
         template_name = 'pages/hakkimda.html'
     ), name='hakkimda'),
